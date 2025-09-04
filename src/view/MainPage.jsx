07a7ea17c79a8840d9374f57e1d6011949a89416
Korneliia Mushak <korneliiapositive@gmail.com>
2025-09-04 21:26:09 +0200

    aboutMe

--- src/view/MainPage.jsx
+++ src/view/MainPage.jsx
@@ -2,6 +2,7 @@ import s from "./mainPage.module.scss";
 import TopNav from "./layots/TopNav/TopNav.jsx";
 import Header from "./layots/Header/Header.jsx";
 import StepByStepSupport from "./components/StepByStepSupport/StepByStepSupport.jsx";
+import AboutMe from "./components/AboutMe/AboutMe.jsx";
 
 function MainPage() {
     const mainWrapper = 1100;
@@ -10,6 +11,7 @@ function MainPage() {
             <TopNav/>
             <Header mainWrapper={mainWrapper}/>
             <StepByStepSupport mainWrapper={mainWrapper}/>
+            <AboutMe mainWrapper={mainWrapper}/>
         </div>
     )
 }
---
+++ src/view/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,14 @@
+import s from "./AboutMe.module.scss";
+
+
+function AboutMe({mainWrapper}) {
+    return (
+        <div className={s.aboutMe}>
+            <div className={s.wrapper} style={{width: mainWrapper}}>
+                s
+            </div>
+        </div>
+    )
+}
+
+export default AboutMe
