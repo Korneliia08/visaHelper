a4e4d83b771bf61246436ad50f4108f4719e1179
Korneliia Mushak <korneliiapositive@gmail.com>
2025-09-08 17:30:50 +0200

    create logo

--- src/view/layots/TopNav/TopNav.jsx
+++ src/view/layots/TopNav/TopNav.jsx
@@ -1,9 +1,13 @@
 import s from "./TopNav.module.scss";
+import logo from "./../../../assets/logo.png";
 
 function TopNav() {
     return (
         <div className={s.topNav}>
-            <div className={s.divForLogo}></div>
+            <div className={s.divForLogo}>
+                <img src={logo} alt="logo" className={s.logo}/>
+                <h5 className={s.logoName}>visa-helper</h5>
+            </div>
             <div className={s.containerForNavAndBtn}>
                 <nav className={s.nav}>
                     <a href="">Про мене</a>
