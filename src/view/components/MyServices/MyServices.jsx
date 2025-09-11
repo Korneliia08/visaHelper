1d3dc5c318a4e5befde4c81fa345b5043164915f
Korneliia Mushak <korneliiapositive@gmail.com>
2025-09-11 20:42:04 +0200

    font family and icon hands

--- src/view/components/MyServices/MyServices.jsx
+++ src/view/components/MyServices/MyServices.jsx
@@ -18,56 +18,63 @@ function MyServices({mainWrapper}) {
                     <div className={s.service}>
                         <div className={s.topBlock}>
                             <FaArrowRight className={s.icArrow}/>
-                            <h4 className={s.title}>Віза</h4>
+                            <h4 className={s.title}>Робоча віза</h4>
                         </div>
-                        <p className={s.description}>
-                            Допомога у підготовці та оформленні візи для подорожей чи навчання.
-                        </p>
+                        <p className={s.description}>Робоча віза – документ для легальної роботи за кордоном.</p>
                     </div>
                     <div className={s.service}>
                         <div className={s.topBlock}>
                             <FaArrowRight className={s.icArrow}/>
-                            <h4 className={s.title}>Карта поляка</h4>
+                            <h4 className={s.title}>Віза по карті поляка</h4>
                         </div>
-                        <p className={s.description}>
-                            Консультація та супровід у отриманні Карти поляка.
-                        </p>
+                        <p className={s.description}>Віза по карті поляка – дозволяє легально перебувати і працювати в
+                            Польщі.</p>
                     </div>
                     <div className={s.service}>
                         <div className={s.topBlock}>
                             <FaArrowRight className={s.icArrow}/>
-                            <h4 className={s.title}>Закордонний паспорт</h4>
+                            <h4 className={s.title}>Реєстрація на першу карту поляка</h4>
                         </div>
-                        <p className={s.description}>
-                            Оформлення та продовження закордонного паспорта для виїзду за кордон.
-                        </p>
+                        <p className={s.description}>Перша карта поляка – підтверджує польське походження та права її
+                            власника.</p>
                     </div>
                     <div className={s.service}>
                         <div className={s.topBlock}>
                             <FaArrowRight className={s.icArrow}/>
-                            <h4 className={s.title}>Номер PESEL</h4>
+                            <h4 className={s.title}>Продовження карти поляка</h4>
                         </div>
-                        <p className={s.description}>
-                            Допомога у реєстрації та отриманні польського номера PESEL.
-                        </p>
+                        <p className={s.description}>Продовження карти поляка – оновлення прав та дозволів для власника
+                            карти.</p>
                     </div>
                     <div className={s.service}>
                         <div className={s.topBlock}>
                             <FaArrowRight className={s.icArrow}/>
-                            <h4 className={s.title}>Медична страховка</h4>
+                            <h4 className={s.title}>Реєстрація на MRG</h4>
                         </div>
-                        <p className={s.description}>
-                            Підбір та оформлення медичної страховки для перебування за кордоном.
-                        </p>
+                        <p className={s.description}>MRG – медична страховка для перебування у Польщі.</p>
                     </div>
                     <div className={s.service}>
                         <div className={s.topBlock}>
                             <FaArrowRight className={s.icArrow}/>
-                            <h4 className={s.title}>Дозвіл на роботу</h4>
+                            <h4 className={s.title}>Заповнення анкети на стали побит</h4>
                         </div>
-                        <p className={s.description}>
-                            Супровід у отриманні дозволу на роботу в Польщі.
-                        </p>
+                        <p className={s.description}>Документ для отримання дозволу на постійне
+                            проживання в Польщі.
+                            роботу.</p>
+                    </div>
+                    <div className={s.service}>
+                        <div className={s.topBlock}>
+                            <FaArrowRight className={s.icArrow}/>
+                            <h4 className={s.title}>Автоцивілка</h4>
+                        </div>
+                        <p className={s.description}>Автоцивілка – обов’язкова страхування для автомобіля.</p>
+                    </div>
+                    <div className={s.service}>
+                        <div className={s.topBlock}>
+                            <FaArrowRight className={s.icArrow}/>
+                            <h4 className={s.title}>Зелена карта</h4>
+                        </div>
+                        <p className={s.description}>Зелена карта – міжнародна страховка для авто за кордоном.</p>
                     </div>
                 </div>
             </div>
