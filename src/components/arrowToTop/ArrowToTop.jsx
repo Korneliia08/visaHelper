3c74637b7e27276824780d893879364d79b538bb
Korneliia Mushak <korneliiapositive@gmail.com>
2025-09-11 20:04:57 +0200

    font family and icon hands

--- src/components/arrowToTop/ArrowToTop.jsx
+++ src/components/arrowToTop/ArrowToTop.jsx
@@ -1,16 +1,38 @@
+import {useEffect, useState} from "react";
 import s from "./ArrowToTop.module.scss";
 import {IoIosArrowUp} from "react-icons/io";
 
-function arrowToTop() {
+function ArrowToTop() {
+    const [visible, setVisible] = useState(false);
+
     const scrollToTop = () => {
         window.scrollTo({
             top: 0,
             behavior: "smooth"
         });
     };
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) { // кнопка з'являється після 50px скролу
+                setVisible(true);
+            } else { // на верху сторінки ховаємо
+                setVisible(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-        <div className={s.arrowToTop} onClick={scrollToTop}><IoIosArrowUp className={s.iconTop}/></div>
-    )
+        <div
+            className={`${s.arrowToTop} ${visible ? s.visible : ""}`}
+            onClick={scrollToTop}
+        >
+            <IoIosArrowUp className={s.iconTop}/>
+        </div>
+    );
 }
 
-export default arrowToTop;
+export default ArrowToTop;
